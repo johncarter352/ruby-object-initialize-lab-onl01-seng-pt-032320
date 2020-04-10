5283ef0da7bfffceb48dda242174d9608942ba51
John Carter <johncarter352@gmail.com>
2020-04-10 02:36:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,22 @@
 class Dog 
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   
   def name=(name)
     @name = name
   end
   
+  def breed=(breed)
+    @breed = breed
+  end
+  
   def name
     @name
-  end 
+  end
+  
+  def breed
+    @breed
+  end
 end
